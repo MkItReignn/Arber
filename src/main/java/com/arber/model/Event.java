b195824cbe04fa6ae72ee1b6ddbcebb09f1bb73a
Alex Kang <akiami.com@gmail.com>
2024-12-25 22:32:13 +1100

    008 - Update Event with new member vairables
    Event now includes the League and MarketTypes available.

--- src/main/java/com/arber/model/Event.java
+++ src/main/java/com/arber/model/Event.java
@@ -1,21 +1,29 @@
 package com.arber.model;
 
+import com.arber.enums.MarketType;
 import com.arber.enums.Sport;
+import com.arber.enums.League;
 import java.time.ZonedDateTime;
+import java.util.List;
 
 public class Event {
     private final String theId;
     private final Sport theSport;
+    private final League theLeague;
     private final String theHomeTeamName;
     private final String theAwayTeamName;
     private final ZonedDateTime theCommenceTime;
+    private final List<MarketType> theMarketTypesAvailable;
 
-    public Event(String aId, Sport aSport, String aHomeTeamName, String aAwayTeamName, ZonedDateTime aCommenceTime) {
+    public Event(String aId, Sport aSport, League theLeague, String aHomeTeamName, String aAwayTeamName,
+                 ZonedDateTime aCommenceTime, List<MarketType> theMarketTypesAvailable) {
         this.theId = aId;
         this.theSport = aSport;
+        this.theLeague = theLeague;
         this.theHomeTeamName = aHomeTeamName;
         this.theAwayTeamName = aAwayTeamName;
         this.theCommenceTime = aCommenceTime;
+        this.theMarketTypesAvailable = theMarketTypesAvailable;
     }
 
     public String getId() {
@@ -41,8 +49,8 @@ public class Event {
     @Override
     public String toString() {
         return String.format(
-                "Event{id='%s', sport='%s', homeTeam='%s', awayTeam='%s', commenceTime='%s'}",
-                theId, theSport, theHomeTeamName, theAwayTeamName, theCommenceTime
+                "Event{id='%s', sport='%s', league='%s', homeTeam='%s', awayTeam='%s', commenceTime='%s', marketTypesAvailable='%s'}",
+                theId, theSport, theLeague, theHomeTeamName, theAwayTeamName, theCommenceTime, theMarketTypesAvailable
         );
     }
 }
