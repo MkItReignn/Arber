ffe391c36035a46a44e0e756bd3eada220ba9382
Alex Kang <akiami.com@gmail.com>
2025-01-12 14:27:15 +1100

    025 - Throw ApiException in all SportsApiClient methods
    It is expected that ApiClients when they make a request, there will be
    some exception thrown from some call.
    
    Universally, all callers of ApiClients must handle the ApiException and
    must expect it.

--- src/main/java/com/arber/api/SportsApiClient.java
+++ src/main/java/com/arber/api/SportsApiClient.java
@@ -1,11 +1,7 @@
 package com.arber.api;
 
-import com.arber.enums.Bookmaker;
-import com.arber.enums.MarketType;
-import com.arber.enums.Sport;
-import com.arber.enums.League;
-import com.arber.enums.Region;
-import com.arber.model.*;
+import com.arber.api.exception.ApiException;
+import com.arber.datamodel.*;
 
 import java.util.List;
 import java.util.Map;
@@ -13,28 +9,26 @@ import java.util.Set;
 
 public interface SportsApiClient {
 
-    Set<Sport> fetchAvailableSports();
+    Set<Sport> fetchAvailableSports() throws ApiException;
 
-    Set<League> fetchAvailableLeagues();
-    Set<League> fetchAvailableLeagues(Sport aSport);
-    Set<LeagueMetadata> fetchAvailableLeagueMetadata(Sport aSport);
+    Set<League> fetchAvailableLeagues() throws ApiException;
+    Set<League> fetchAvailableLeagues(Sport aSport) throws ApiException;
+    Set<LeagueMetadata> fetchLeagueMetadata(Sport aSport) throws ApiException;
 
-    Set<EventMetadata> fetchAvailableEvents();
-    Set<EventMetadata> fetchAvailableEvents(League aLeague);
+    Set<EventMetadata> fetchEventMetadata() throws ApiException;
+    Set<EventMetadata> fetchEventMetadata(League aLeague) throws ApiException;
 
-    Map<League, Map<EventId, MarketToBookmakers>> fetchMarketToBookmakers(Sport aSport);
-    Map<League, Map<EventId, MarketToBookmakers>> fetchMarketToBookmakers(Sport aSport, Region aRegion);
-    Map<EventId, MarketToBookmakers> fetchMarketToBookmakers(League aLeague);
-    Map<EventId, MarketToBookmakers> fetchMarketToBookmakers(League aLeague, Region aRegion);
+    Map<League, Map<EventId, MarketToBookmakers>> fetchMarketToBookmakers(Sport aSport) throws ApiException;
+    Map<League, Map<EventId, MarketToBookmakers>> fetchMarketToBookmakers(Sport aSport, Set<Region> aRegions) throws ApiException;
+    Map<EventId, MarketToBookmakers> fetchMarketToBookmakers(League aLeague) throws ApiException;
+    Map<EventId, MarketToBookmakers> fetchMarketToBookmakers(League aLeague, Set<Region> aRegions) throws ApiException;
 
-    Set<MarketType> fetchMarketTypes(String anEventId);
-    Set<MarketType> fetchMarketTypes(String anEventId, Region aRegion);
+    Set<MarketType> fetchMarketTypes(EventId anEventId) throws ApiException;
+    Set<MarketType> fetchMarketTypes(EventId anEventId, Set<Region> aRegions) throws ApiException;
 
-    Set<Bookmaker> fetchBookmakers(String anEventId, MarketType aMarketType);
-    Set<Bookmaker> fetchBookmakers(String anEventId, MarketType aMarketType, Region aRegion);
+    Set<Bookmaker> fetchBookmakers(EventId anEventId, MarketType aMarketType) throws ApiException;
+    Set<Bookmaker> fetchBookmakers(EventId anEventId, MarketType aMarketType, Set<Region> aRegions) throws ApiException;
 
-    List<Odds> fetchOdds(String anEventId, MarketType aMarketType);
-    List<Odds> fetchOdds(String anEventId, MarketType aMarketType, Region aRegion);
-
-    boolean isApiAvailable();
+    List<OddsMetadata> fetchOddsMetadata(EventId anEventId, MarketType aMarketType) throws ApiException;
+    List<OddsMetadata> fetchOddsMetadata(EventId anEventId, MarketType aMarketType, Set<Region> aRegions) throws ApiException;
 }
